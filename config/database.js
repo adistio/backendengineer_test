a6658974018a435de551cf54a74acef0da51c4a6
Adi Sulistiono <adisulistiono67@gmail.com>
2022-10-15 17:09:27 +0700

    add env db

--- config/database.js
+++ config/database.js
@@ -1,8 +1,11 @@
 import { Sequelize } from "sequelize";
- 
-const db = new Sequelize('backendengineer_test', 'root', '',{
-    host: 'localhost',
-    dialect: 'mysql'
+import dotenv from 'dotenv'
+dotenv.config({path: './.env'}); 
+
+const db = new Sequelize(process.env.MYSQL_DBNAME, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD,{
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
+    port: process.env.MYSQL_PORT
 });
  
 export default db;
